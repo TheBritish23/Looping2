74458f916eef1261d8d33edb1dcc35d8d849ab2f
Soro Cat <sorousha326@gmail.com>
2019-06-03 14:54:46 -0700

    add changes'

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+var dashify = function(sentence){
+  console.log(sentence)
+ return;
+}
+
 $(document).ready(function() {
   $('form#list').submit(function(event){
-    event.preventDefault();
+     event.preventDefault()
+
+  var words = $('#words').val()
+
+  var newsentence = dashify(words);
+
+  $('output').append(newsentence)
+
+  var dashify = ['A', 'E', 'I', 'O', 'U'];
+  for (var index = 0; index < languages.length; index += 1) {
+    alert('I love ' + dashify[index] + '!');
+  }
+
+
+  });
+});
